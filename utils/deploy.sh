65d097c8981b82244306723396fe239d0e398f4c
Alex Shoronov <alexshoronov@gmail.com>
2017-06-12 20:01:50 +0300

    Adding logs to deploy-script

--- utils/deploy.sh
+++ utils/deploy.sh
@@ -5,6 +5,8 @@ function get_tmp {
 	echo ".`cat /dev/urandom | env LC_CTYPE=C tr -dc a-z0-9 | head -c 16`"
 }
 
+echo "Running deploy..."
+
 SRC_URL='git@github.com:frontendu/frontendu.github.io'
 NPM_VERSION=$1
 TMP_PATH=`get_tmp`
@@ -14,28 +16,39 @@ if [ -z $NPM_VERSION ]; then
 	NPM_VERSION='patch'
 fi
 
+echo "Incrementing version to ${NPM_VERSION}..."
+
 npm version $NPM_VERSION -m "Version bumped to %s"
 
-git push origin dev
-git push --tags
+echo "Pushing version and tags..."
+
+git push origin dev -q
+git push --tags -q
 
 SITE_VERSION=`node -e "
 	console.log(require('./package.json').version)
 "`
 
-npm run build:prod
+echo "Builing site..."
 
-git clone $SRC_URL $TMP_PATH
-cd $TMP_PATH;
-git checkout master
+npm run build:prod -- --silent
 
-git rm -r ./
+echo "Prepare for publishing site..."
+
+git clone $SRC_URL $TMP_PATH -q
+cd $TMP_PATH > /dev/null;
+git checkout master -q
+git rm -r ./ -q
+
+echo "Copying new content of site..."
 
 cp -r ../$DIST_PATH/* ./
-git add -A
+git add -A -q
+
+echo "Publishing site..."
 
 git commit -m "Version of site bumped to ${SITE_VERSION}" || rm -rf $TMP_PATH
 
-git push origin master
+git push origin master -q
 
 rm -rf $TMP_PATH
