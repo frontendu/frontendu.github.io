ef7142fd1df438951967cd32019a7b5af46ea8c8
Aleksandr Shoronov <ashoronov@yandex-team.ru>
2019-02-02 21:49:04 +0500

    Revert "🎄 Added christmas sound"
    This reverts commit 05750ce2456afa9ca9ec8b96cd5eea4372ad3387.

--- src/pages/index.js
+++ src/pages/index.js
@@ -17,8 +17,8 @@ import ball from '../assets/ball.png'
 import logo from '../assets/logo.svg';
 import coin from '../assets/coin.svg';
 
-const LogoWrapper = styled.div`
-	cursor: pointer;
+const LogoWrapper = styled(Link)`
+	display: block;
 	position: relative;
 	margin: 0 auto;
 	width: 320px;
@@ -178,10 +178,6 @@ class Landing extends React.Component {
 		postponeLoadFont();
 	}
 
-	onLogoClick() {
-		this.audio && this.audio.play();
-	}
-
 	render() {
 		return (
 			<StaticQuery
@@ -205,8 +201,7 @@ class Landing extends React.Component {
 						<link rel="shortcut icon" href="/favicon.ico" />
 					</Helmet>
 					<Hero>
-						<audio src="yoyoyo.ogg" ref={audio => this.audio = audio} />
-						<LogoWrapper onClick={() => this.onLogoClick()} title="Счастливого Нового Года!">
+						<LogoWrapper to="/">
 							<ChristmasBall />
 							<StyledLogoLink>
 								<Logo src={logo} />
